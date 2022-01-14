a6b0772b95526d3cbedea2ee84b85622c373ef50
Brandon <brandonhk47@gmail.com>
2022-01-14 13:38:32 -0600

    Downloaded version currently onboard the raspberry pi, look at current version and check it out

--- SensorPackage.py
+++ SensorPackage.py
@@ -47,6 +47,10 @@ vl53l1x_dict={
 sensor_list=[vl53l1x_dict,tmp117_dict] # This list should be from lowest address to highest so the data markers are correct
 logged_data=["time(s"] # Array for Data that will be logged
 activeSensors=[] # Array of active sensors
+#nav_time=0
+#nav_lat=0
+#nav_long=0
+#nav_alt=0
 ##############################################
 # Calculate the 2's complement of a number
 def twos_comp(val, bits):
@@ -169,12 +173,24 @@ def initialize_mavlink_log():
     master.wait_heartbeat()
     print("Heartbeat Received")
     master.mav.request_data_stream_send(master.target_system, master.target_component,mavutil.mavlink.MAV_DATA_STREAM_ALL, 1,0)
+    time.sleep(0.1)
+    master.mav.command_long_send(master.target_system,master.target_component,mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL,0,
+                                 mavutil.mavlink.MAVLINK_MSG_ID_GLOBAL_POSITION_INT,1e6/1,2,0,0,0,0)
+    time.sleep(0.1)
+    master.mav.command_long_send(master.target_system,master.target_component,mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL,0,
+                                 mavutil.mavlink.MAVLINK_MSG_ID_GPS_STATUS,1e6/1,2,0,0,0,0)
+    time.sleep(0.1)
     #master.mav.request_data_stream_send(master.target_system, master.target_component,mavutil.mavlink.MAV_DATA_STREAM_POSITION, 1,1)
     #master.mav.request_data_stream_send(master.target_system, master.target_component,mavutil.mavlink.MAV_DATA_STREAM_RAW_CONTROLLER, 1,1)
-    logged_data.append("Navio2_Time_ms")
-    logged_data.append("Navio2_latitude")  
-    logged_data.append("Navio2_longitude")
-    logged_data.append("Navio2_Relative_Altitude")
+    logged_data.append("Navio2_Time (GLOBAL_POSITION_INT, ms)")
+    logged_data.append("Navio2_latitude (GLOBAL_POSITION_INT, degE7)")  
+    logged_data.append("Navio2_longitude (GLOBAL_POSITION_INT, degE7)")
+    logged_data.append("Navio2_Relative_Altitude (GLOBAL_POSITION_INT, mm)")
+    logged_data.append("Navio2_Altitude (GLOBAL_POSITION_INT, mm)")
+    logged_data.append("Navio2_Ground_X_Speed (GLOBAL_POSITION_INT, cm/s)")
+    logged_data.append("Navio2_Ground_Y_Speed (GLOBAL_POSITION_INT, cm/s)")
+    logged_data.append("Navio2_Ground_Z_Speed (GLOBAL_POSITION_INT, cm/s)")
+    logged_data.append("Navio2_Heading (GLOBAL_POSITION_INT, cdeg)")
 
 # def read_mavlink_data(search):
 #     msg=master.recv_match().to_dict()
@@ -184,14 +200,15 @@ def initialize_mavlink_log():
 
 #########################################
 # Adjustable Variables
-test_mode=0 # This is whether we ignore whether the system is armed or not and performs the data logging
-
+test_mode=1 # This is whether we ignore whether the system is armed or not and performs the data logging
+check_once=0
+bad_data=0
 #########################################
 # Main Code
 while True:
     test_time=0
     distanceInitialize = 0
-
+    check_global_pos_int=0
     check_Available_Sensors()
     initialize_mavlink_log()
 
@@ -217,8 +234,6 @@ while True:
             sense_time=time.time()
             
             if i == 0x29:
-                if test_mode == 1: 
-                    print("Performing Distance Sensor Measurement")
                 try:
                     if distanceInitialize==1:
                         distance=measure_distance()
@@ -232,8 +247,6 @@ while True:
                     print("Sensor Error Has Occured, Ignoring Distance Readings Until Sensor is Fixed")
                     data.append("")
             elif i == 0x48:
-                if test_mode == 1: 
-                    print("Performing temperature sensor measurement")
                 try:
                     temperature = read_temp()
                     data.append(temperature)
@@ -245,10 +258,7 @@ while True:
                     print("Performing Death")
                 rangeItUp=to_die()
                 #data.append(rangeItUp)
-            if test_mode == 1:     
-                print("Time to complete one sensor: "+ str(round(time.time()-sense_time,3)))
-        if test_mode == 1:         
-            print("Time to complete all sensors: "+ str(round(time.time()-test_time,3)))
+                
         #print(temperature, "C")
                 #["Navio2_Time_ms","Navio2_latitude","Navio2_longitude","Navio2_Relative_Altitude"])
     #     master.mav.command_long_send(master.target_system,master.target_component,
@@ -259,26 +269,66 @@ while True:
     #         pass
         test_time=time.time()
         try:
-            master.mav.command_long_send(master.target_system,master.target_component,mavutil.mavlink.MAV_CMD_REQUEST_MESSAGE,0,33, 0, 0, 0, 0, 0, 0)
-            master.mav.command_long_send(master.target_system, master.target_component,
-                                         mavutil.mavlink.MAV_CMD_REQUEST_MESSAGE, 0, 62, 0, 0, 0, 0, 0, 0)
-            time.sleep(1)
+             
+            #master.mav.command_long_send(master.target_system, master.target_component,
+                                    #mavutil.mavlink.MAV_CMD_REQUEST_MESSAGE, 0, 33, 0, 0, 0, 0, 0, 0)
+                
+            #master.mav.command_long_send(master.target_system,master.target_component,mavutil.mavlink.MAV_CMD_REQUEST_MESSAGE,0,33, 0, 0, 0, 0, 0, 0)
+            time.sleep(.25)
             msg=master.recv_match().to_dict()
+            try:
+                if msg['mavpackettype']=='BAD_DATA':
+                    bad_data=bad_data+1
+                    print("Consecutive Bad Data: ",bad_data)
+                    print(msg)
+            except:
+                pass
+            if check_once==0:
+                        base_time=msg['time_boot_ms']
+                        check_once=1
+            try:
+                over_time=msg['time_boot_ms']
+            except:
+                pass
+            #if test_mode == 1:
+                #print(msg)
             print(msg['mavpackettype'])
-            if test_mode == 1: 
-                print(msg['mavpackettype'])
+            try:
+                print((over_time-base_time)/1000)
+                print(data[0]-(over_time-base_time)/1000)
+                if (data[0]-(over_time-base_time)/1000) > 5:
+                            overtime=1
+                            while overtime==1:
+                                
+                                msg=master.recv_match().to_dict()
+                                try:
+                                    over_time=msg['time_boot_ms']
+                                except:
+                                    pass
+                                #print(data[0]-(over_time-base_time)/1000)
+                                if (data[0]-(over_time-base_time)/1000) < 1:
+                                    overtime=0
+            except:
+                pass
+            
             if msg['mavpackettype'] == 'NAV_CONTROLLER_OUTPUT':
                 try:
                     print(msg['wp_dist'])
                 except:
                     print("Time Failed to Load")
-
+            
             if msg['mavpackettype'] == 'GLOBAL_POSITION_INT':
                 try:
+                    bad_data=0
                     nav_time=msg['time_boot_ms']
                     nav_lat = msg['lat']
                     nav_long = msg['lon']
-                    nav_alt = msg['relative_alt'] / 1000
+                    nav_rel_alt = msg['relative_alt']
+                    nav_alt = msg['alt']
+                    nav_vx = msg['vx']
+                    nav_vy = msg['vy']
+                    nav_vz = msg['vz']
+                    nav_hdg = msg['hdg']
                     #print(msg['time_boot_ms'])
                 except:
                     print("Global Position Failed ")
@@ -286,7 +336,12 @@ while True:
             data.append(nav_time)
             data.append(nav_lat)
             data.append(nav_long)
+            data.append(nav_rel_alt)
             data.append(nav_alt)
+            data.append(nav_vx)
+            data.append(nav_vy)
+            data.append(nav_vz)
+            data.append(nav_hdg)
                 
         except:
             #print("Exception in Mavlink")
@@ -294,26 +349,22 @@ while True:
             data.append("")
             data.append("")
             data.append("")
+            data.append("")
+            data.append("")
+            data.append("")
+            data.append("")
+            data.append("")
             
-        if test_mode == 1:    
-            print("Time to Read Mavlink Data: "+ str(round(time.time()-test_time,3)))
             
-        test_time=time.time()
         try:
             print(data)
         except:
             pass
-        if test_mode == 1: 
-            print("Time to print data: "+ str(round(time.time()-test_time,3)))
         
         test_time=time.time()
         log_data_file(data)
+        #time.sleep(0.1)
         
-        if test_mode == 1: 
-            print("Time to log data: "+ str(round(time.time()-test_time,3)))
-            print("Time to complete one iteration: "+ str(round(time.time()-itr_time,3)))
-            #time.sleep(1)
-            print('\r\n')
             
     while master.motors_armed()==0:
         # Since we are disarmed, we are going to reset the active sensors and create a new log file name that is just going to be the current time
@@ -322,7 +373,8 @@ while True:
         logged_data=["time(s"] # Array for Data that will be logged
         activeSensors=[] # Array of active sensors
         #print("Are we armmed? :" + str(master.motors_armed()))
-        master.mav.command_long_send(master.target_system,master.target_component,mavutil.mavlink.MAV_CMD_REQUEST_MESSAGE,0,33, 0, 0, 0, 0, 0, 0)
+        
+        #master.mav.command_long_send(master.target_system,master.target_component,mavutil.mavlink.MAV_CMD_REQUEST_MESSAGE,0,33, 0, 0, 0, 0, 0, 0)
         time.sleep(.1)
         try:
             msg=master.recv_match().to_dict()
