b2baa6caec257817365240ca2160329c2d698afd
Brandon <brandonhk47@gmail.com>
2022-01-18 11:34:23 -0600

    [200~Edits - Removed Sensor List and Sensor Dictionary since they are no longer needed to initialize sensors - moved data appending to inside measurement function - added try/except to TMP117 and VL53L1X so that if you unplug the sensor after it has already been initialized, it will maintain the blank spacing and continue to run. If you plug the sensor back in, the TMP117 will work however right now the VL53L1X would need to be reintialized after being plugged back in - added BME/CCS811 Environmental Breakout Sensor code to main Sensor Code - Added a function to create a number of blank appends to data instead of doing a line of code for each blank in the data section - shifted sensor initialization inside the main loop so that when over you are disarmed you can change out sensors without having to recycle power - added a check for armed to look at the heartbeat, as part of this I added a counter to check how many times i receive a base_mode of less than 128 from the heartbeat message as for some reason while being armed it sends 0s and 129s to me even though I am looking at 129 as my arm state. - **ISSUE** right now when the code runs for the first time, it creates a blank file then makes another to start data logging. The reason behind this is because I have the code that does initialization outside of my armed and disarmed while loops. - Fixed issue above by creating a separate function to initialize mavlink then another function that initializes the log inside the armed while loop

--- SensorPackage.py
+++ SensorPackage.py
@@ -1,5 +1,5 @@
 #######################################################
-# Current Date of Version: 1/17/2022
+# Current Date of Version: 1/18/2022
 #######################################################
 # The service's name is sensorPackage.service
 
@@ -23,8 +23,8 @@ import sys
 from enum import Enum
 from datetime import datetime
 from math import log10, floor
-# for BME280: "sudo pip install pimoroni-bme280"
-# for CCS811: "sudo pip install sparkfun-qwiic-ccs811"
+from bme280 import BME280 # for BME280: "sudo pip install pimoroni-bme280"
+import qwiic_ccs811 # for CCS811: "sudo pip install sparkfun-qwiic-ccs811"
 # for Github: "sudo apt-get install git"
 from pymavlink import mavutil  # Install in terminal via: "sudo python -m pip install --upgrade pymavlink"
 import qwiic_vl53l1x  # Install in terminal via: "sudo pip install sparkfun-qwiic-vl53l1x"
@@ -38,27 +38,68 @@ tmp117_reg_config = 0x01
 # Name for Log File, Gives Date and Time
 log_file = datetime.now().strftime('%Y%m%d_%H%M%S') + ".txt"
 ##############################################
-# Dicitionaries for All Sensors, You just add a sensor here and inside the sensor_list array
-tmp117_dict = {
-    "Name": "TMP117 Temperature Sensor",
-    "Address": 0x48,
-    "Logged Data": "Temperature (C)"
-}
-vl53l1x_dict = {
-    "Name": "VL53L1X Distance Sensor",
-    "Address": 0x29,
-    "Logged Data": "Distance (mm)"
-}
-##############################################
-sensor_list = [vl53l1x_dict, tmp117_dict]  # This list should be from lowest address to highest so the data markers are correct
 logged_data = ["time(s"]  # Array for Data that will be logged
 activeSensors = []  # Array of active sensors
+data=[]
 ########################################################
 # Threading Testing
-
+bus = SMBus(1)
+class bme280_ccs811_sensor:
+    def __init__(self):
+        #  # Keep an eye on this, I might need to do a general call for the Bus to the entire program
+        global logged_data
+        # might need to delcare these sensors global
+        try:
+            global bme280
+            global ccs811Sense
+            ccs811Sense = qwiic_ccs811.QwiicCcs811()
+            bme280 = BME280(i2c_dev=bus,i2c_addr=0x77)
+            ccs811Sense.begin()
+            logged_data.append("BME280 Temperature (C)")
+            logged_data.append("BME280 Pressure (hPa)")
+            logged_data.append("BME280 Humidity (%)")
+            logged_data.append("BME280 Altitude (ft)")
+            logged_data.append("CCS811 CO2 (ppm)")
+            logged_data.append("CCS811 tVOC (ppb)")
+            self._running = True
+        except:
+            print("BME280 and CCS811 Sensor not Detected")
+            self._running = False
+            return
+        
+        
+        
+    def get_ccs811_bme280_data(self):
+        try:
+            if self._running:
+                global data
+                temperature = bme280.get_temperature()
+                data.append(temperature)
+                pressure = bme280.get_pressure()
+                data.append(pressure)
+                humidity = bme280.get_humidity()
+                data.append(humidity)
+                altitude=bme280.get_altitude()
+                data.append(altitude)
+                ccs811Sense.set_environmental_data(humidity, temperature)
+                if ccs811Sense.data_available():
+                    ccs811Sense.read_algorithm_results()
+                    data.append(ccs811Sense.CO2)
+                    data.append(ccs811Sense.TVOC)
+                else:
+                    append_Blanks(2)
+        except:
+            if self._running:
+                print("Connection to Environmental Sensor Lost")
+                append_Blanks(6)
+                
+def append_Blanks(num_blank):
+    global data
+    for i in range(num_blank):
+        data.append("")
 class VL53l1x_distance_sensor:
     def __init__(self):
-        bus = SMBus(1)  # Keep an eye on this, I might need to do a general call for the Bus to the entire program
+          # Keep an eye on this, I might need to do a general call for the Bus to the entire program
         global logged_data
         try:
             global rangeSensor
@@ -72,22 +113,27 @@ class VL53l1x_distance_sensor:
             print("VL53L1X Sensor not Detected")
             self._running = False
             return
-        bus.close()
+        
 
     def measure_distance(self):
-        if self._running:
-            # rangeSensor=qwiic_vl53l1x.QwiicVL53L1X()
-            # rangeSensor.sensor_init()
-            rangeSensor.start_ranging()
-            time.sleep(0.005)
-            distance = rangeSensor.get_distance()
-            time.sleep(0.005)
-            rangeSensor.stop_ranging()
-            return distance
+        try:
+            if self._running:
+                global data
+                rangeSensor.start_ranging()
+                time.sleep(0.005)
+                distance = rangeSensor.get_distance()
+                time.sleep(0.005)
+                rangeSensor.stop_ranging()
+                data.append(distance)
+                return distance
+        except:
+            if self._running:
+                print("Connection to VL53L1X Lost")
+                append_Blanks(1)
 
 class Tmp117_Sensor:
     def __init__(self):
-        bus = SMBus(1)  # Keep an eye on this, I might need to do a general call for the Bus to the entire program
+          # Keep an eye on this, I might need to do a general call for the Bus to the entire program
         global logged_data
         try:
             bus.read_byte_data(tmp117_addr, 0x00)
@@ -98,7 +144,7 @@ class Tmp117_Sensor:
             print("TMP117 Sensor not Detected")
             self._running = False
             return
-        bus.close()
+        
 
     def terminate(self):
         self._running = False
@@ -110,32 +156,39 @@ class Tmp117_Sensor:
         return val
 
     def tempReading(self):
-        if self._running:
-            # Initialize I2C (SMBus)
-            bus = SMBus(1)
+        try:
+            if self._running:
+                # Initialize I2C (SMBus)
+                
+                global data
+                # Read the CONFIG register (2 bytes)
+                val = bus.read_i2c_block_data(tmp117_addr, tmp117_reg_config, 2)
+                # print("Old CONFIG:", hex(val[0]),hex(val[1]))
 
-            # Read the CONFIG register (2 bytes)
-            val = bus.read_i2c_block_data(tmp117_addr, tmp117_reg_config, 2)
-            # print("Old CONFIG:", hex(val[0]),hex(val[1]))
+                # Set to 4 Hz sampling (CR1, CR0 = 0b10)
+                val[1] = val[1] & 0b00111111
+                val[1] = val[1] | (0b10 << 6)
 
-            # Set to 4 Hz sampling (CR1, CR0 = 0b10)
-            val[1] = val[1] & 0b00111111
-            val[1] = val[1] | (0b10 << 6)
+                # Write 4 Hz sampling back to CONFIG
+                bus.write_i2c_block_data(tmp117_addr, tmp117_reg_config, val)
 
-            # Write 4 Hz sampling back to CONFIG
-            bus.write_i2c_block_data(tmp117_addr, tmp117_reg_config, val)
-
-            # Read CONFIG to verify that we changed it
-            val = bus.read_i2c_block_data(tmp117_addr, tmp117_reg_config, 2)
-            # Print out temperature every second
-            # Read temperature registers
-            val = bus.read_i2c_block_data(tmp117_addr, tmp117_reg_temp, 2)
-            temp_c = (val[0] << 8) | (val[1] >> 0)
-            temp_c = self.twos_comp(temp_c, 16)
-            # Convert registers value to temperature (C)
-            temp_c = temp_c * 0.0078125
-            bus.close()
-            return temp_c
+                # Read CONFIG to verify that we changed it
+                val = bus.read_i2c_block_data(tmp117_addr, tmp117_reg_config, 2)
+                # Print out temperature every second
+                # Read temperature registers
+                val = bus.read_i2c_block_data(tmp117_addr, tmp117_reg_temp, 2)
+                temp_c = (val[0] << 8) | (val[1] >> 0)
+                temp_c = self.twos_comp(temp_c, 16)
+                # Convert registers value to temperature (C)
+                temp_c = temp_c * 0.0078125
+                
+                data.append(temp_c)
+                return temp_c
+        except:
+            if self._running:
+                print("Connection to TMP117 Lost")
+                append_Blanks(1)
+            
 
 ##############################################
 
@@ -167,7 +220,7 @@ def initialize_log_file():
 
 #########################################
 # So here I want to select what Mavlink Data to bring in to the Data Log
-def initialize_mavlink_log():
+def initialize_mavlink():
     global master
     master = mavutil.mavlink_connection("/dev/ttyS0", baud=115200)
     print("Waiting for Heartbeat")
@@ -186,6 +239,9 @@ def initialize_mavlink_log():
     time.sleep(0.1)
     # master.mav.request_data_stream_send(master.target_system, master.target_component,mavutil.mavlink.MAV_DATA_STREAM_POSITION, 1,1)
     # master.mav.request_data_stream_send(master.target_system, master.target_component,mavutil.mavlink.MAV_DATA_STREAM_RAW_CONTROLLER, 1,1)
+
+def initialize_mavlink_log():
+    global logged_data
     logged_data.append("Navio2_Time (GLOBAL_POSITION_INT, ms)")
     logged_data.append("Navio2_latitude (GLOBAL_POSITION_INT, degE7)")
     logged_data.append("Navio2_longitude (GLOBAL_POSITION_INT, degE7)")
@@ -195,8 +251,6 @@ def initialize_mavlink_log():
     logged_data.append("Navio2_Ground_Y_Speed (GLOBAL_POSITION_INT, cm/s)")
     logged_data.append("Navio2_Ground_Z_Speed (GLOBAL_POSITION_INT, cm/s)")
     logged_data.append("Navio2_Heading (GLOBAL_POSITION_INT, cdeg)")
-
-
 # def read_mavlink_data(search):
 #     msg=master.recv_match().to_dict()
 #     if msg['mavpackettype'] == 'GLOBAL_POSITION_INT':
@@ -205,46 +259,66 @@ def initialize_mavlink_log():
 
 #########################################
 # Adjustable Variables
-test_mode = 1  # This is whether we ignore whether the system is armed or not and performs the data logging
+test_mode = 0  # This is whether we ignore whether the system is armed or not and performs the data logging
 check_once = 0
 bad_data = 0
 #########################################
 
-tempSense = Tmp117_Sensor()
-distSense = VL53l1x_distance_sensor()
-print(distSense.measure_distance())
-print(tempSense.tempReading())
+# tempSense = Tmp117_Sensor()
+# distSense = VL53l1x_distance_sensor()
+# envSense = bme280_ccs811_sensor()
+# print(distSense.measure_distance())
+# print(tempSense.tempReading())
 
-
+heartBeatArmed=0
+initialize_items=0
+initialize_mavlink()
 # Main Code
 while True:
-    test_time = 0
-    distanceInitialize = 0
-    check_global_pos_int = 0
-
-    initialize_mavlink_log()
-
-    # print("Are we armed? :" + str(master.motors_armed()))
-    print("Data Being Logged: ")
-    print(logged_data)
-    print("\n")
-
-    initialize_log_file()
 
     start_time = time.time()
-
-    while master.motors_armed() == 128 or test_mode == 1:
-        # print("Are we armmed? :" + str(master.motors_armed()))
+    while heartBeatArmed != 129:
+        # Since we are disarmed, we are going to reset the active sensors and create a new log file name that is just going to be the current time
+        # That way when we arm up again it will make a new file.
+        log_file = datetime.now().strftime('%Y%m%d_%H%M%S') + ".txt"
+        logged_data = ["time(s"]  # Array for Data that will be logged
+        initialize_items=0
+        # master.mav.command_long_send(master.target_system,master.target_component,mavutil.mavlink.MAV_CMD_REQUEST_MESSAGE,0,33, 0, 0, 0, 0, 0, 0)
+        time.sleep(.1)
+        try:
+            msg = master.recv_match().to_dict()
+            print(msg['mavpackettype'])
+            if msg['mavpackettype']== 'HEARTBEAT':
+                heartBeatArmed=msg['base_mode']
+                #print(heartBeatArmed)
+        except:
+            pass
+        #time.sleep(.5)
+        
+    while heartBeatArmed==129 or test_mode == 1:
+        if initialize_items==0: 
+            distanceInitialize = 0
+            check_global_pos_int = 0
+            tempSense = Tmp117_Sensor()
+            distSense = VL53l1x_distance_sensor()
+            envSense = bme280_ccs811_sensor()
+            initialize_mavlink_log()
+            print("Data Being Logged: ")
+            print(logged_data)
+            print("\n")
+            initialize_log_file()
+            initialize_items=1
+        
+        
         data = []
-
         data.append(round(time.time() - start_time, 3))
-        test_time = time.time()
-        itr_time = time.time()
+        # Get VL53L1X Data
         distance = distSense.measure_distance()
-        data.append(distance)
+        # Get TMP117 Data
         temperature = tempSense.tempReading()
-        data.append(temperature)
-        test_time = time.time()
+        # Get BME280 and CCS811 data
+        envSense.get_ccs811_bme280_data()
+        
         try:
 
             # master.mav.command_long_send(master.target_system, master.target_component,
@@ -267,12 +341,11 @@ while True:
                 over_time = msg['time_boot_ms']
             except:
                 pass
-            # if test_mode == 1:
-            # print(msg)
-            print(msg['mavpackettype'])
+            if test_mode == 1:
+                print(msg['mavpackettype'])
             try:
-                print((over_time - base_time) / 1000)
-                print(data[0] - (over_time - base_time) / 1000)
+                #print((over_time - base_time) / 1000)
+                #print(data[0] - (over_time - base_time) / 1000)
                 if (data[0] - (over_time - base_time) / 1000) > 5:
                     overtime = 1
                     while overtime == 1:
@@ -293,7 +366,19 @@ while True:
                     print(msg['wp_dist'])
                 except:
                     print("Time Failed to Load")
-
+                    
+            if msg['mavpackettype']== 'HEARTBEAT':
+                #print(msg)
+                if msg['base_mode'] == 0:
+                    heartBeatCount=heartBeatCount+1
+                else:
+                    heartBeatCount=0
+                    
+                if heartBeatCount>3:
+                    heartBeatArmed=0
+                #heartBeatArmed=msg['base_mode']
+                #print(heartBeatCount)
+                
             if msg['mavpackettype'] == 'GLOBAL_POSITION_INT':
                 try:
                     bad_data = 0
@@ -322,38 +407,10 @@ while True:
 
         except:
             # print("Exception in Mavlink")
-            data.append("")
-            data.append("")
-            data.append("")
-            data.append("")
-            data.append("")
-            data.append("")
-            data.append("")
-            data.append("")
-            data.append("")
-
+            append_Blanks(9)
         try:
             print(data)
         except:
             pass
-
-        test_time = time.time()
         log_data_file(data)
         # time.sleep(0.1)
-
-    while master.motors_armed() == 0:
-        # Since we are disarmed, we are going to reset the active sensors and create a new log file name that is just going to be the current time
-        # That way when we arm up again it will make a new file.
-        log_file = datetime.now().strftime('%Y%m%d_%H%M%S') + ".txt"
-        logged_data = ["time(s"]  # Array for Data that will be logged
-        activeSensors = []  # Array of active sensors
-        # print("Are we armmed? :" + str(master.motors_armed()))
-
-        # master.mav.command_long_send(master.target_system,master.target_component,mavutil.mavlink.MAV_CMD_REQUEST_MESSAGE,0,33, 0, 0, 0, 0, 0, 0)
-        time.sleep(.1)
-        try:
-            msg = master.recv_match().to_dict()
-            print(msg['mavpackettype'])
-        except:
-            pass
-        # time.sleep(.5)
